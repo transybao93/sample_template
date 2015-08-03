6d4279236137f52d4eeac8f510d857cae3b9e8c2
transybao <IT_coder_93@yahoo.com.vn>
2015-08-03 20:32:16 +0700

    :boom::camel: Added .gitattributes

---
+++ app.js
@@ -0,0 +1,13 @@
+﻿	
+	var express = require('express'),
+	app = express();
+	app.set('views', './views'),
+	app.set('view engine', 'jade'),
+	app.use(express.static(__dirname + '/public')),
+	
+	app.get('/', function (req, res) {
+	  res.render('index', { title: 'Welcome to my website', name: 'My name is Bao'});
+	});
+	app.listen(4000);
+	console.log('Testing Nodejs template');
+	
